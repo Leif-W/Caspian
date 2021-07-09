bdec719a15748d9726bb26bed86398fccb7fa3c3
Leif W <Leif-W@users.noreply.github.com>
2021-07-09 13:54:07 -0400

    🟢(fea) Build git ignore from API and custom rules

---
+++ util/gitignore.js
@@ -0,0 +1,58 @@
+'use strict';
+
+const fs = require('fs');
+const https = require('https');
+const fileName = '.gitignore';
+const headerText = `\
+# AUTO GENERATED FILE - DO NOT EDIT
+
+# Edit the \`# Custom Rules\` section near the end of the file \`util/gitignore.js\`.
+# Then rebuild this \`.gitignore\` file with \`node util/gitignore.js\`.
+
+# Auto generated from Toptal's https://gitignore.io/
+`;
+const bodyUrl = 'https://www.toptal.com/developers/gitignore/api/' + [
+	'android',
+	'dotenv',
+	'firebase',
+	'linux',
+	'macos',
+	'node',
+	'notepadpp',
+	'react',
+	'vim',
+	'visualstudio',
+	'visualstudiocode',
+	'windows',
+	'xcode',
+	'yarn',
+	'zsh',
+].join(',');
+const footerText = `\
+# Custom Rules
+
+# .env
+.env*
+!.env*.example
+
+# Vercel
+.vercel
+`;
+https.get(bodyUrl, (res) => {
+	let bodyText = '';
+
+	res.on('error', (err) => {
+		console.error('util/gitignore.js - https.get - res - error: ', err.message);
+	});
+
+	res.on('data', (data) => {
+		bodyText += data;
+	});
+
+	res.on('end', () => {
+                fs.writeFileSync(fileName, headerText + bodyText + footerText);
+		console.log('util/gitignore.js - https.get ok - written: ' + fileName);
+	});
+}).on('error', (err) => {
+	console.error('util/gitignore.js - https.get - error: ', err.message);
+});
