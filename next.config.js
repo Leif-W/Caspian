90882b922b6ef73f23a138187458369aab01d2a6
Leif W <Leif-W@users.noreply.github.com>
2021-07-19 10:37:50 -0400

    🟤(cho) Tell WatchPack to ignore Vim's swap files

--- next.config.js
+++ next.config.js
@@ -1,5 +1,26 @@
 'use strict';
 
 module.exports = {
+	webpack: (config) => {
+		[
+			// Vim Swap files
+			'**/[._]*.s[a-v][a-z]',
+			'**/[._]*.sw[a-p]',
+			'**/[._]s[a-rt-v][a-z]',
+			'**/[._]ss[a-gi-z]',
+			'**/[._]sw[a-p]',
+			// Vim Session
+			'**/Session{,x}.vim',
+			// Vim Temporary
+			'**/.netrwhist',
+			// Vim tag files
+			'**/tags',
+			// Vim Persistent undo
+			'**/[._]*.un~',
+		].forEach((item) => {
+			config.watchOptions.ignored.push(item);
+		});
+		return config;
+	},
 	webpack5: true,
 };
