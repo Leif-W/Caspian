dd94fbf7534157ab8fd5b6c3a2561a07350ab4d3
Leif W <Leif-W@users.noreply.github.com>
2021-07-20 19:12:11 -0400

    🟣(sty) Header: fix logo, add search box and icon

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,14 +2,33 @@
 
 import Image from 'next/image';
 import logoImg from '../../public/logo.svg';
+import {
+	SearchIcon,
+} from '@heroicons/react/outline';
 
 const Header = () => {
 	return(
 		<header>
 			{/* top nav */}
-			<div className='bg-caspian_blue-pitch flex flex-grow items-center p-1 py-2 sm:flex-grow-0'>
+			<div
+				className='
+					bg-caspian_blue-pitch
+					flex
+					flex-grow
+					items-center
+					p-1
+					py-2
+				'
+			>
 				{/* img */}
-				<div className='flex flex-grow items-center mt-2'>
+				<div
+					className='
+						flex
+						items-center
+						mt-2
+						sm:flex-grow-0
+					'
+				>
 					<Image
 						alt='Caspian logo'
 						className='cursor-pointer'
@@ -19,7 +38,41 @@ const Header = () => {
 						width={150}
 					/>
 				</div>
+
 				{/* search */}
+				<div
+					className='
+						bg-caspian_gold-light
+						cursor-pointer
+						flex-grow
+						h-10
+						hidden
+						hover:bg-caspian_gold-default
+						items-center
+						rounded-md
+						sm:flex
+					'
+				>
+					<input
+						className='
+							flex-grow
+							flex-shrink
+							focus:outline-none
+							h-full
+							p-2
+							rounded-l-md
+							w-6
+						'
+						type='text'
+					/>
+					<SearchIcon
+						className='
+							h-12
+							p-4
+						'
+					/>
+				</div>
+
 				{/* right side */}
 			</div>
 
