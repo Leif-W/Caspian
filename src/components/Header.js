1ef0c7dcdbebcc561cfa60f756696c610647c28e
Leif W <Leif-W@users.noreply.github.com>
2021-07-23 12:18:19 -0400

    🟣(sty) Finish initial Header design mockup

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,9 @@
 import Image from 'next/image';
 import logoImg from '../../public/logo.svg';
 import {
+	MenuIcon,
 	SearchIcon,
+	ShoppingCartIcon,
 } from '@heroicons/react/outline';
 
 const Header = () => {
@@ -42,12 +44,11 @@ const Header = () => {
 				{/* search */}
 				<div
 					className='
-						bg-caspian_gold-light
+						sm:bg-caspian_gold-light
 						cursor-pointer
 						flex-grow
 						h-10
-						hidden
-						hover:bg-caspian_gold-default
+						sm:hover:bg-caspian_gold-default
 						items-center
 						rounded-md
 						sm:flex
@@ -59,8 +60,10 @@ const Header = () => {
 							flex-shrink
 							focus:outline-none
 							h-full
+							hidden
 							p-2
 							rounded-l-md
+							sm:inline
 							w-6
 						'
 						type='text'
@@ -69,15 +72,160 @@ const Header = () => {
 						className='
 							h-12
 							p-4
+							hidden
+							sm:inline
 						'
 					/>
 				</div>
 
 				{/* right side */}
+				<div
+					className='
+						flex
+						items-center
+						mx-4
+						sm:mx-6
+						sm:space-x-6
+						space-x-4
+						text-white
+						text-xs
+						whitespace-nowrap
+					'
+				>
+					{/* account */}
+					<div className='link'>
+						<p>Hello, New User</p>
+						<p className='f-b'>Account & Lists</p>
+					</div>
+
+					{/* orders */}
+					<div className='link'>
+						<p>Returns</p>
+						<p className='f-b'>& Orders</p>
+					</div>
+
+					{/* basket */}
+					<div
+						className='
+							flex
+							items-center
+							link
+							relative
+						'
+					>
+						<span
+							className='
+								absolute
+								bg-caspian_gold-light
+								font-bold
+								h-4
+								md:right-10
+								right-0
+								rounded-full
+								text-black
+								text-center
+								top-0
+								w-4
+							'
+						>0</span>
+						<ShoppingCartIcon className='h-10'/>
+						<p
+							className='
+								f-b
+								hidden
+								md:inline
+								mt-2
+							'
+						>Basket</p>
+					</div>
+				</div>
 			</div>
 
 			{/* bottom nav */}
-			<div>
+			<div
+				className='
+					bg-caspian_blue-dark
+					flex
+					items-center
+					p-2
+					pl-6
+					space-x-3
+					text-white
+					text-sm
+				'
+			>
+				<p
+					className='
+						flex
+						items-center
+						link
+					'
+				>
+					<MenuIcon
+						className='
+							h-6
+							mr-1
+						'
+					/>
+					All
+				</p>
+				<p className='link'>Prime Video</p>
+				<p className='link'>Caspian Business</p>
+				<p className='link'>Today's Deals</p>
+				<p
+					className='
+						hidden
+						link
+						lg:inline-flex
+					'
+				>
+					Electronics
+				</p>
+				<p
+					className='
+						hidden
+						lg:inline-flex
+						link
+					'
+				>
+					Food & Grocery
+				</p>
+				<p
+					className='
+						hidden
+						lg:inline-flex
+						link
+					'
+				>
+					Prime
+				</p>
+				<p
+					className='
+						hidden
+						lg:inline-flex
+						link
+					'
+				>
+					Buy Again
+				</p>
+				<p
+					className='
+						hidden
+						lg:inline-flex
+						link
+					'
+				>
+					Shopper Toolkit
+				</p>
+				<p
+					className='
+						hidden
+						lg:inline-flex
+						link
+					'
+				>
+					Health & Personal Care
+				</p>
 			</div>
 		</header>
 	);
