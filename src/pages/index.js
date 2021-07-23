2892bde1e8cebcee38e7e4d4da74c8c36fb67738
Leif W <Leif-W@users.noreply.github.com>
2021-07-23 12:08:29 -0400

    🟣(sty) Add favicon.ico to index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,18 @@
 'use strict';
 
+import Head from 'next/head';
 import Header from '../components/Header';
 
 const Home = () => {
 	return(
 		<>
+			<Head>
+				<link
+					rel='icon'
+					src='/favicon.ico'
+				/>
+			</Head>
+
 			{/* Header */}
 			<Header />
 
